78653a82e60ca3ecbcd05b5aca42cce9df9c977e
Jinggong Zheng <kingsley@masters-MacBook-Pro.local>
2015-08-20 15:39:57 -0600

    Updated method return file.

--- lib/fakes/oauth/yodlee/yodlee_fake_api.rb
+++ lib/fakes/oauth/yodlee/yodlee_fake_api.rb
@@ -4,30 +4,30 @@ module Fakes
       #preroute = '/yodlee'
 
       # fake cob session token
-      app.post "/services/srest/restserver/v1.0/authenticate/coblogin" do
+      app.post '/services/srest/restserver/v1.0/authenticate/coblogin' do
         json_response 200, 'coblogin.json', 'yodlee'
       end
 
       # fake user session token
-      app.post "/services/srest/restserver/v1.0/authenticate/login" do
+      app.post '/services/srest/restserver/v1.0/authenticate/login' do
         json_response 200, 'login.json', 'yodlee'
       end
 
       # fake add site account
-      app.post "/services/srest/restserver/v1.0/jsonsdk/SiteAccountManagement/addSiteAccount1" do
+      app.post '/services/srest/restserver/v1.0/jsonsdk/SiteAccountManagement/addSiteAccount1' do
         json_response 200, 'add_site_account_1_added.json', 'yodlee'
       end
 
       # fake get site refresh info
-      app.post "/services/srest/restserver/v1.0/jsonsdk/Refresh/getSiteRefreshInfo" do
-        json_response 200, 'get_site_refresh_info_added.json', 'yodlee'
+      app.post '/services/srest/restserver/v1.0/jsonsdk/Refresh/getSiteRefreshInfo' do
+        json_response 200, 'get_site_refresh_info_added', 'yodlee'
       end
 
-      app.post "/services/srest/restserver/v1.0/jsonsdk/SiteAccountManagement/removeSiteAccount" do
+      app.post '/services/srest/restserver/v1.0/jsonsdk/SiteAccountManagement/removeSiteAccount' do
         json_response 200, 'remove_site_account.json', 'yodlee'
       end
 
-      app.post "/services/srest/restserver/v1.0/jsonsdk/DataService/getItemSummariesForSite" do
+      app.post '/services/srest/restserver/v1.0/jsonsdk/DataService/getItemSummariesForSite' do
         json_response 200, 'get_item_summaries_for_site.json', 'yodlee'
       end
 
